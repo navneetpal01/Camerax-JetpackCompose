1c64a3d9b5673ace772c42b088850b95f12bbe0d
navneetpal01 <navneetpalgithub@gmail.com>
2024-02-03 11:12:09 +0530

    Second Commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,4 +66,19 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+
+    //CameraX
+    val cameraxVersion = "1.3.0-rc01"
+
+    implementation("androidx.camera:camera-core:$cameraxVersion")
+    implementation("androidx.camera:camera-camera2:$cameraxVersion")
+    implementation("androidx.camera:camera-lifecycle:$cameraxVersion")
+    implementation("androidx.camera:camera-video:$cameraxVersion")
+
+    implementation("androidx.camera:camera-view:$cameraxVersion")
+    implementation("androidx.camera:camera-extensions:$cameraxVersion")
+
+    //Accompanist
+    implementation("com.google.accompanist:accompanist-permissions:$0.32.0")
 }
