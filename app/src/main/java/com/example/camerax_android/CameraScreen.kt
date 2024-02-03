21da62fc47b211c69ed0911e4bfb9faf3692977f
navneetpal01 <navneetpalgithub@gmail.com>
2024-02-03 23:47:41 +0530

    Fifth

--- app/src/main/java/com/example/camerax_android/CameraScreen.kt
+++ app/src/main/java/com/example/camerax_android/CameraScreen.kt
@@ -1,11 +1,76 @@
 package com.example.camerax_android
 
+import android.annotation.SuppressLint
+import android.graphics.Color
+import android.view.ViewGroup.LayoutParams.MATCH_PARENT
+import android.widget.LinearLayout
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageProxy
+import androidx.camera.view.LifecycleCameraController
+import androidx.camera.view.PreviewView
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.viewmodel.compose.viewModel
 
 @Composable
-fun CameraScreen(
-    viewModel : CameraViewModel = viewModel()
-){
-
+fun CameraScreen(){
+    CameraContent()
+}
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+private fun CameraContent(){
+    val context = LocalContext.current
+    val lifeCycleOwner = LocalLifecycleOwner.current
+    val cameraController = remember { LifecycleCameraController(context)}
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        floatingActionButton = {
+            ExtendedFloatingActionButton(
+                onClick = {
+                    val mainExecutor = ContextCompat.getMainExecutor(context)
+                    cameraController.takePicture(mainExecutor,object : ImageCapture.OnImageCapturedCallback(){
+                        override fun onCaptureSuccess(image: ImageProxy) {
+                            image.close()
+                        }
+                    })
+                }
+            ) {
+                Text(text = "Take Photo")
+            }
+        }
+    ){
+        paddingValues : PaddingValues ->
+        AndroidView(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues),
+            factory = { context ->
+            PreviewView(context).apply {
+                layoutParams = LinearLayout.LayoutParams(MATCH_PARENT, MATCH_PARENT)
+                setBackgroundColor(Color.BLACK)
+                scaleType = PreviewView.ScaleType.FILL_START
+            }.also{ previewView ->
+                previewView.controller = cameraController
+                cameraController.bindToLifecycle(lifeCycleOwner)
+            }
+        })
+        
+    }
+}
+@Preview
+@Composable
+private fun Preview_CameraContent(){
+    CameraContent()
 }
--- app/src/main/java/com/example/camerax_android/CameraViewModel.kt
+++
@@ -1,6 +0,0 @@
-package com.example.camerax_android
-
-import androidx.lifecycle.ViewModel
-
-class CameraViewModel : ViewModel() {
-}
